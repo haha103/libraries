05e1b50fe68e823a549e76e5adeafd4e3244c574
Liu Wei <wliu272@163.com>
2015-03-21 08:45:27 +0800

    hello world

---
+++ cxx/hello.cxx
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <thread>
+#include <mutex>
+#include <algorithm>
+#include <map>
+#include <vector>
+
+using namespace std;
+
+int main ( int argc, char ** argv )
+{
+    cout << "hello" << endl;
+    return 0;
+}
